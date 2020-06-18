bc5490e04895ed71e6633e8145eb6297e58bd9d8
EliorCohen <elior123456123123@gmail.com>
2020-06-18 17:09:40 +0300

    update

--- app/src/main/java/elior/com/infrastructure/ClassesPackage/FavoritesPlaces.java
+++ app/src/main/java/elior/com/infrastructure/ClassesPackage/FavoritesPlaces.java
@@ -7,14 +7,12 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import elior.com.infrastructure.AdapterPackage.PlacesListAdapterFav;
-import elior.com.infrastructure.OthersPackage.NetworkDataProviderFavorites;
 import elior.com.infrastructure.R;
 import elior.com.infrastructure.RoomFavoritesPackage.PlacesViewModelFavorites;
 
 public class FavoritesPlaces extends AppCompatActivity {
 
     private PlacesViewModelFavorites mPlacesViewModelFavorites;
-    private NetworkDataProviderFavorites networkDataProviderFavorites;
     private PlacesListAdapterFav mAdapterFavorites;
     private RecyclerView recyclerView;
 
@@ -24,17 +22,11 @@ public class FavoritesPlaces extends AppCompatActivity {
         setContentView(R.layout.activity_favorites);
 
         initUI();
-        getData();
         myRecyclerView();
     }
 
     private void initUI() {
         recyclerView = findViewById(R.id.recyclerViewFav);
-        networkDataProviderFavorites = new NetworkDataProviderFavorites();
-    }
-
-    private void getData() {
-        networkDataProviderFavorites.getPlacesByLocation();
     }
 
     private void myRecyclerView() {
